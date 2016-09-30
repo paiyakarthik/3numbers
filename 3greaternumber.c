ea530b42670757b9b5d5872f57593d3aa609934f
paiyakarthik <karthik27061995@gmail.com>
2016-09-30 17:13:21 +0530

    Add files via upload

---
+++ 3greaternumber.c
@@ -0,0 +1,19 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+cout<<"enter the  3 numbers";
+cin>>a>>b>>c;
+a<b?cout<<"the b is grater than a":cout<<"the b is less then a";
+if(a<c)
+{
+cout<<"a is less c";
+if(b<c)
+cout<<"the b is less c";
+else
+cout<<"the cis grater then a&b";
+
+}
+return 0;
+}
